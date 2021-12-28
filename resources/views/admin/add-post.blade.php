6fac53e3f131c47f4f9e1d4cab37f35ae6e1e997
nenad <zmajnex@gmail.com>
2021-12-28 20:50:33 +0100

    Working on add new post form refactoring

--- resources/views/admin/add-post.blade.php
+++ resources/views/admin/add-post.blade.php
@@ -1,31 +1,4 @@
-<script>
- /*   let previewList = "preview-blog-list";
-    let previewPost = "preview-blog-post"
-    function showBlogListImage() {
-        return {
-            showPreview(event) {
-                if (event.target.files.length > 0) {
-                    var src = URL.createObjectURL(event.target.files[0]);
-                    var preview = document.getElementById(previewList );
-                    preview.src = src;
-                    preview.style.display = "block";
-                }
-            }
-        }
-    }
-    function showBlogPostImage() {
-        return {
-            showPreview(event) {
-                if (event.target.files.length > 0) {
-                    var src = URL.createObjectURL(event.target.files[0]);
-                    var preview = document.getElementById(previewPost);
-                    preview.src = src;
-                    preview.style.display = "block";
-                }
-            }
-        }
-    }*/
-</script>
+
 <x-admin-layout>
     <div class=" container w-full ">
         <form method="POST" action="#" class="w-full">
